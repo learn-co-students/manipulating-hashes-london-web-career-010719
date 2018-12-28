8211f0f5d6364e56a234ba729797e469572653b6
github username <github email address>
2018-12-28 09:00:45 +0100

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,6 +14,22 @@ def first_challenge
   }
 
   #your code here
+  contacts.each do |person, data|
+      
+      data.each do |attribute, value|
+          
+          if attribute == :favorite_icecream_flavors
+              
+               value.delete_if do |ice_cream|
+                   
+                   ice_cream == "strawberry" 
+                   
+                end
+              end
+          
+          end
+      
+      end
 
 
   #remember to return your newly altered contacts hash!
